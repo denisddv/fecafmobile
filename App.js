f47eef58720ac614e27947463b75192cd239bfb0
denisddv <113071741+denisddv@users.noreply.github.com>
2025-12-05 16:59:15 -0300

    feat: Add App.js entry point with Redux and Navigation setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { store } from './src/store/store';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
+    </Provider>
+  );
+}
